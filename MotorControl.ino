98e98fb6c8ac2c7c24d90b867908665d75bb4bb4
manuelp <48811126+Mani9971@users.noreply.github.com>
2021-09-09 00:01:19 +0200

    Added LED strip

--- MotorControl.ino
+++ MotorControl.ino
@@ -1,4 +1,26 @@
 //Initializing LED Pin
+#include <WS2812B.h>
+
+#define NUM_LEDS 7
+
+WS2812B strip = WS2812B(NUM_LEDS);
+uint8_t LEDGamma[] = {
+    0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+    0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  1,  1,  1,  1,
+    1,  1,  1,  1,  1,  1,  1,  1,  1,  2,  2,  2,  2,  2,  2,  2,
+    2,  3,  3,  3,  3,  3,  3,  3,  4,  4,  4,  4,  4,  5,  5,  5,
+    5,  6,  6,  6,  6,  7,  7,  7,  7,  8,  8,  8,  9,  9,  9, 10,
+   10, 10, 11, 11, 11, 12, 12, 13, 13, 13, 14, 14, 15, 15, 16, 16,
+   17, 17, 18, 18, 19, 19, 20, 20, 21, 21, 22, 22, 23, 24, 24, 25,
+   25, 26, 27, 27, 28, 29, 29, 30, 31, 32, 32, 33, 34, 35, 35, 36,
+   37, 38, 39, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 50,
+   51, 52, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 66, 67, 68,
+   69, 70, 72, 73, 74, 75, 77, 78, 79, 81, 82, 83, 85, 86, 87, 89,
+   90, 92, 93, 95, 96, 98, 99,101,102,104,105,107,109,110,112,114,
+  115,117,119,120,122,124,126,127,129,131,133,135,137,138,140,142,
+  144,146,148,150,152,154,156,158,160,162,164,167,169,171,173,175,
+  177,180,182,184,186,189,191,193,196,198,200,203,205,208,210,213,
+215,218,220,223,225,228,231,233,236,239,241,244,247,249,252,255 };
 
 int EN_A = PA0;      //Enable pin for first motor
 int IN1 = PC15;       //control pin for first motor
@@ -23,13 +45,17 @@ void setup() {
   digitalWrite(IN1, LOW);
   digitalWrite(IN2, HIGH);
 
+  strip.begin();// Sets up the SPI
+  strip.show();// Clears the strip, as by default the strip data is set to all LED's off.
+
 }
 
 void loop() {
     recvWithStartEndMarkers();
     percentage = getNewData();
-
+      
     setSpeed(percentage);
+    pulseWhite(5); 
 }
  
 void recvWithStartEndMarkers() {
@@ -76,5 +102,23 @@ void setSpeed(int percentage)
 {
   pwm = map(percentage, 0, 100, 0, 255);
   analogWrite(EN_A, pwm);
+}
+
+void pulseWhite(uint8_t wait) {
+  for(int j = 0; j < 256 ; j++){
+      for(uint16_t i=0; i<strip.numPixels(); i++) {
+          strip.setPixelColor(i, strip.Color(LEDGamma[j],LEDGamma[j],LEDGamma[j] ) );
+        }
+        delay(wait);
+        strip.show();
+      }
+
+  for(int j = 255; j >= 0 ; j--){
+      for(uint16_t i=0; i<strip.numPixels(); i++) {
+          strip.setPixelColor(i, strip.Color(LEDGamma[j],LEDGamma[j],LEDGamma[j] ) );
+        }
+        delay(wait);
+        strip.show();
+      }
 }
   
